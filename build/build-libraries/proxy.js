07cbc93db3125c00062ab9dde0454096b9bb88c5
Benjamin Atkin <ben@resources.co>
2024-01-06 23:52:05 -0800

    respond to connect request and attempt to tunnel connection

--- build/build-libraries/proxy.js
+++ build/build-libraries/proxy.js
@@ -1,13 +1,30 @@
-Deno.serve({ port: 3000 }, async request => {
-  console.log({request})
-  // const url = new URL(request.url)
-  // url.search = search
-  // const headers = new Headers(request.headers)
-  // headers.set('Host', url.hostname)
-  // return fetch(url, {
-  //   method: request.method,
-  //   headers,
-  //   body: request.body,
-  //   redirect: 'manual',
-  // })
-})
+async function handleHttp(conn) {
+  conn.setKeepAlive(true)
+  let serverUrl
+  let promise
+  const httpConn = Deno.serveHttp(conn)
+  const e = await httpConn.nextRequest()
+  console.log({headers: e.request.headers})
+  if (e.request.method === 'CONNECT') {
+    serverUrl = e.request.url
+    console.log({serverUrl})
+    await e.respondWith(new Response(null, {
+      status: 200,
+      statusText: 'OK',
+      headers: {Connection: 'Keep-Alive'},
+    }))
+  }
+  const serverUrlInfo = new URL(serverUrl)
+  try {
+    const connectArgs = {hostname: serverUrlInfo.hostname, port: Number(serverUrlInfo.port)}
+    const outConn = await Deno.connect(connectArgs)
+    await conn.readable.pipeTo(outConn.writable)
+    await outConn.readable.pipeTo(conn.writable)
+  } catch (err) {
+    console.error('Error creating connection', err)
+  }
+}
+
+for await (const conn of Deno.listen({ port: 3000 })) {
+  handleHttp(conn)
+}
