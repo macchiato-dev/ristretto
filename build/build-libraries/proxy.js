be3bb3cb99af5e84fc5becd1108a03100704366d
Benjamin Atkin <ben@resources.co>
2024-01-07 13:51:18 -0800

    get it proxying and installing a package on npm

--- build/build-libraries/proxy.js
+++ build/build-libraries/proxy.js
@@ -1,8 +1,17 @@
+async function forward(outConn, writer) {
+  for await (const chunk of outConn.readable) {
+    console.log(`read chunk of ${chunk.byteLength} bytes from outbound connection`)
+    await writer.write(chunk)
+  }
+}
+
 async function handleHttp(conn) {
   let pos = 0
   const arr = new Uint8Array(512)
   let remaining
   let message
+  let outConn
+  let outWriter
   for await (const chunk of conn.readable) {
     if (message === undefined) {
       arr.set(chunk, pos)
@@ -15,11 +24,20 @@ async function handleHttp(conn) {
         const messageEnd = match.index + match[0].length
         message = decoded.slice(0, messageEnd)
         remaining = arr.slice(new TextEncoder().encode(message).byteLength, pos)
+        const proxyUrl = message.match(/CONNECT (\S+) HTTP/)[1].split(':')
+        const connectArgs = {hostname: proxyUrl[0], port: Number(proxyUrl[1])}
+        console.log({connectArgs})
+        outConn = await Deno.connect(connectArgs)
+        outConn.setKeepAlive(true)
         const writer = await conn.writable.getWriter()
         await writer.write(new Uint8Array(new TextEncoder().encode('HTTP/1.1 200 Connection established\r\n\r\n')))
+        outWriter = await outConn.writable.getWriter()
+        // TODO: send remaining
+        forward(outConn, writer)
       }
     } else {
       console.log(`read chunk of ${chunk.byteLength} bytes after sending response`)
+      await outWriter.write(chunk)
     }
   }
 }
